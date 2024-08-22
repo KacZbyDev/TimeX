c5cb5f4e1b330088edadd9db299defec7d86b605
Bumbi <progamerro12345@gmail.com>
2024-08-22 09:56:59 +0300

    add timer functionality

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,30 @@
-var x:string = "1";
-document.write(x);
-var x2:number = 21;
+const refreshTime:number = 100;
+const decimals = 1
+
+let refreshInterval:number
+
+let miliseconds:number = 1000;
+let ul = document.querySelector("#timer")
+
+if(ul) {
+    ul!.textContent = milisecondsToSecondsFormat(miliseconds)
+    
+    refreshInterval = setInterval(updateTime, refreshTime)
+}
+
+function updateTime(): void {
+    miliseconds -= refreshTime;  
+    if(miliseconds > 0)
+        ul!.textContent = milisecondsToSecondsFormat(miliseconds)
+    else
+        refreshIntervalFinished()
+}
+
+function refreshIntervalFinished() :void{
+    ul!.textContent = "finished"
+    clearInterval(refreshInterval)
+}
+
+function milisecondsToSecondsFormat(miliseconds:number):string{
+    return (miliseconds / 1000).toFixed(decimals)
+}
