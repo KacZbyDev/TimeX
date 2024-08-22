7c4b7cc271d6b341897304e168cae1baaa4ac688
Bumbi <progamerro12345@gmail.com>
2024-08-22 12:04:58 +0300

    Retrieve typescript script

--- dest/script.js
+++ dest/script.js
@@ -1,4 +1,25 @@
 "use strict";
-var x = "1";
-document.write(x);
-var x2 = 21;
+const refreshTime = 100;
+const decimals = 1;
+let refreshInterval;
+let miliseconds = 1000;
+let ul = document.querySelector("#timer");
+if (ul) {
+    ul.textContent = milisecondsToSecondsFormat(miliseconds);
+    refreshInterval = setInterval(updateTime, refreshTime);
+}
+function updateTime() {
+    miliseconds -= refreshTime;
+    if (miliseconds > 0)
+        ul.textContent = milisecondsToSecondsFormat(miliseconds);
+    else
+        refreshIntervalFinished();
+}
+function refreshIntervalFinished() {
+    ul.textContent = "finished";
+    clearInterval(refreshInterval);
+}
+function milisecondsToSecondsFormat(miliseconds) {
+    return (miliseconds / 1000).toFixed(decimals);
+}
+//# sourceMappingURL=script.js.map
