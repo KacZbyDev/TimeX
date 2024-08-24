3fbb673c790b18be11337ca3aeed2bcb772ad2d3
Bumbi <progamerro12345@gmail.com>
2024-08-24 09:47:10 +0300

    Add custom color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ module.exports = {
   content: ['./html/*.html'],
   theme: {
     extend: {
+      //TODO do we use this?
       boxShadow: {
         'custom-amber': '0px 0px 12px 2px'
       },
+
+      colors: {
+          "blue-primary": "#2C599D",
+          "blue-secondary": "#5B84C4",
+
+          "orange-primary": "#F98125",
+          "orange-secondary": "#FB9B50"
+      }
     },
   },
   plugins: [],
