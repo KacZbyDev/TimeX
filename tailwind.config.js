5c1fcfcf65b6aafc13f2f04f32f50c8ba438fc91
Manu <emanuel.bumbuta@gmail.com>
2024-08-25 07:35:43 +0300

    Mess with the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: ['./html/*.html'],
   theme: {
+    //Do we put the colors here so they are the only ones we use?
+
     extend: {
       //TODO do we use this?
       boxShadow: {
@@ -9,12 +11,16 @@ module.exports = {
       },
 
       colors: {
-          "blue-primary": "#2C599D",
-          "blue-secondary": "#5B84C4",
-
-          "orange-primary": "#F98125",
-          "orange-secondary": "#FB9B50"
-      }
+        "blue-primary": "#2C599D",
+        "blue-secondary": "#5B84C4",
+        "light-blue-primary": "#80beed",//maybe a bit too light
+        "light-blue-secondary": "#d2EBFF",
+        "dark-blue-primary": "#193A6F",//very dark
+        "dark-blue-secondary": "#11224D",
+  
+        "orange-primary": "#F98125",
+        "orange-secondary": "#FB9B50"
+      },
     },
   },
   plugins: [],
