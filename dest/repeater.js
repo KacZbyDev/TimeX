61d466052afe070a0e1fd8306f07ae454cd3de7e
Kacper Zbyrad <135231060+KacZbyDev@users.noreply.github.com>
2024-09-11 17:49:40 +0200

    Delete dest directory

--- dest/repeater.js
+++
@@ -1,9 +0,0 @@
-export class Subtimer {
-    constructor(element, positionInList) {
-        this.element = element;
-        this.positionInList = positionInList;
-        this.totalRepeats = parseInt(element.firstChild.firstChild.textContent);
-        this.currentRepeats = parseInt(element.firstChild.lastChild.textContent);
-    }
-}
-//# sourceMappingURL=repeater.js.map
