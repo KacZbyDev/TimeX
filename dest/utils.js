7b3d61b09f908cdce1f0a90af42403ebb2dae978
Kacper Zbyrad <135231060+KacZbyDev@users.noreply.github.com>
2024-09-11 17:53:57 +0200

    Delete modal/dest directory

--- dest/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const decimals = 1;
-function milisecondsToSecondsFormat(miliseconds) {
-    return (miliseconds / 1000).toFixed(decimals);
-}
-exports.default = milisecondsToSecondsFormat;
-//# sourceMappingURL=utils.js.map
