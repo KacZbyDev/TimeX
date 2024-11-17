7c90eac4347d53e02adb8fcf096fb35b35d528d7
Kacper Zbyrad <maustriacki@gmail.com>
2024-11-17 15:00:47 +0100

    adding sql structure to database

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -8,5 +8,22 @@ const db = new sqlite3.Database(dbPath,(err:any)=>{
         console.log("connected to database")
     }
 });
-
+db.serialize(()=>{
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(60) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        CREATE TABLE IF NOT EXISTS timers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            content TEXT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+        `)
+})
 module.exports = db;
